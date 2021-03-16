9799595f81c11361f9e1c5f39aef5ae8e2806a84
anaelleray <anaelleray7@gmail.com>
2021-03-16 10:03:29 +0100

    start build list

--- src/service/fontAwsomeService.ts
+++ src/service/fontAwsomeService.ts
@@ -1,4 +1,4 @@
-import { faTimesCircle } from "@fortawesome/free-regular-svg-icons";
+import { faTimesCircle,  } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 
