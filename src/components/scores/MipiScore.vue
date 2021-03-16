8c4e9452153d03df1d5792f1c1db0a9a8241b86d
matthias dubocage <dubocage.matthias@gmail.com>
2021-03-16 15:44:38 +0100

    [ feature-score-MIPI Ajoute du component MIPI ]
    Signed-off-by: matthias dubocage <dubocage.matthias@gmail.com>

--- src/components/scores/MipiScore.vue
+++ src/components/scores/MipiScore.vue
@@ -4,7 +4,7 @@
         <div class="input1">
             <div>Age</div>
             <div class="input2">
-                <input type="text">
+                <input type="text" v-on:change="calcul" v-model="years">
                 <div>years</div>
             </div>
         </div>
@@ -12,15 +12,15 @@
         <div class="input1">
             <div>ECOG</div>
             <div class="input2">
-                <div class="" ><div class="" >0-1</div></div>
-                <div class="" ><div class="" >2-4</div></div>
+                <div id="ecog-1" class="ecog-selected" v-on:click="select(1)"><div class="" >0-1</div></div>
+                <div id="ecog-2" class="" v-on:click="select(2)"><div class="" >2-4</div></div>
             </div>
         </div>
 
         <div class="input1">
             <div>Serum LDH</div>
             <div class="input2">
-                <input type="text" >
+                <input type="text" v-on:change="calcul" v-model="serumLDH">
                 <div>U/L</div>
             </div>
         </div>
@@ -31,7 +31,7 @@
                 <div>According to your lab's normal values</div>
             </div>
             <div class="input2">
-                <input type="text" value="160" >
+                <input type="text" value="160"  v-on:change="calcul" v-model="upperLimitLDH">
                 <div>U/L</div>
             </div>
         </div>
@@ -39,7 +39,7 @@
         <div class="input1">
             <div>WBC</div>
             <div class="input2">
-                <input type="text">
+                <input type="text" v-on:change="calcul" v-model="wbc">
                 <div>
                     <div>× 10⁹ cells/L</div>
                     <div hidden>× 10³ cells/µL</div>
@@ -53,12 +53,12 @@
                 <div>Optional, for MIPIb</div>
             </div>
             <div class="input2">
-                <input type="text">
+                <input id="ki" type="text" v-on:change="calcul">
                 <div>%</div>
             </div>
         </div>
 
-        <div>Result:</div>
+        <div>Result:{{result}}</div>
 
     </div>
 </template>
@@ -71,26 +71,88 @@ export default Vue.extend({
     {
         return {
             result: null,
+            years: null,
+            ecog: 1,
+            serumLDH: null,
+            upperLimitLDH: null,
+            wbc: null
         }
     },
+
     mounted()
     {
 
     },
+
     methods: {
 
+        select : function(value){
+            this.ecog = value
+            let b = document.getElementsByClassName('ecog-selected');
+            let ecog1 = document.getElementById('ecog-'+value);
+            if(b){
+                if( b[0] != ecog1 ){
+                    b[0].classList.remove("ecog-selected");
+                    ecog1.classList.add("ecog-selected");
+                }
+            }
+        },
+
+        calcul : function(){
+            let ki = document.getElementById('ki')
+            let valueKi = parseFloat((<HTMLInputElement>ki).value)
+            if(valueKi ){
+                if(this.isNumeric(valueKi) && this.numInInterval(valueKi,0,100)){
+                    this.formulKi();
+                }else{
+                    this.formul();
+                }
+            }else{
+                this.formul();
+            }
+        },
+
+        isNumeric: function (value){
+            if(!isNaN(value)){
+                return true
+            }else{
+                return false
+            }
+        },
+
+        numInInterval:function(value,min,max){
+            if(value >= min && value <= max){
+                return true
+            }else{
+                return false
+            }
+        },
+
+        formul : function (){
+            this.result  =  (0.3535 * this.years) + 0.6978
+            if(this.ecog > 1){
+                 this.result += (1.367 * Math.log10(this.serumLDH / this.upperLimitLDH ) + 0.9393 * Math.log10(this.wbc))
+            }
+        },
+
+        formulKi : function (){
+
+        },
    }
 })
 </script>
 
 <style>
+
     .input1{
         display: flex;
         justify-content: space-around;
         align-items: center;
     }
+
     .input2{
         display: flex;
         justify-content: space-between;
     }
+
 </style>
