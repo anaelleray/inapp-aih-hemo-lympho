0ec6a089e0b4bdae67a075a47fbfee2de82968b5
nerses <nerseshak@gmail.com>
2021-03-17 12:10:14 +0100

    important add Condition in ContentPage

--- src/components/scores/MaladieDeWaldenstromScore.vue
+++ src/components/scores/MaladieDeWaldenstromScore.vue
@@ -1,52 +1,17 @@
 <template>
-  <div class="score-1">
-    <h2>MALT Lymphoma prognosisAAA (MALT-IPI)</h2>
-    <div>
-      <table>
-        <tr>
-          <td>Estimate prognosis in MALT lymphoma</td>
-
-          <td>1. Age?</td>
-          <td>
-            <button style="background: green">Need Change 70 years</button>
-          </td>
-          <td>
-            <button style="background: green">≥70 years</button>
-          </td>
-        </tr>
-
-        <td>2. LDH?</td>
-        <td>
-          <button style="background: green">Normal</button>
-        </td>
-        <td>
-          <button style="background: green">Elevated</button>
-        </td>
-
-        <td>3. Stage?</td>
-        <td>
-          <button style="background: green">Stage I/II</button>
-        </td>
-        <td>
-          <button style="background: green">Stage III/IV</button>
-        </td>
-      </table>
-    </div>
-    <input type="text" value="0" />
+  <div>
+    <div>AAAAAAAAAAAAAAAAAA</div>
   </div>
-
-
 </template>
 
-<script lang="ts" >
+<script lang="ts">
 import Vue from "vue";
 export default Vue.extend({
-  name: "Maladie de WaldenstromAAA",
+  name: "Maladie de Waldenstrom",
   data() {
-    return {
-      result: null,
-    };
+    return {};
   },
+
   mounted() {},
 });
 </script>
--- src/views/ContentPage.vue
+++ src/views/ContentPage.vue
@@ -1,91 +1,96 @@
 <template>
-    <div class="content-page">
-        <keep-alive>
-            <component v-bind:is="currentComponent"></component>
-        </keep-alive>
-    </div>
+  <div class="content-page">
+    <keep-alive>
+      <component v-bind:is="currentComponent"></component>
+    </keep-alive>
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue"
+import Vue from "vue";
 
-import DataService from "@/service/DataService"
-import CriteresGelfScore from "@/components/scores/CriteresGelfScore.vue"
-import FlipiScore from "@/components/scores/FlipiScore.vue"
-import MipiScore from "@/components/scores/MipiScore.vue"
-import CllBallScore from "@/components/scores/CllBallScore.vue"
-import BinetScore from "@/components/scores/BinetScore.vue"
-import MaladieDeWaldenstromScore from "@/components/scores/MaladieDeWaldenstromScore.vue"
-import MatutesRmhScore from "@/components/scores/MatutesRmhScore.vue"
-import NccnIpiScore from "@/components/scores/NccnIpiScore.vue"
-import SmolderingPrognosisScore from "@/components/scores/SmolderingPrognosisScore.vue"
-import CllIpiScore from "@/components/scores/CllIpiScore.vue"
-import CnsIpiScore from "@/components/scores/CnsIpiScore.vue"
-import RIpiScore from "@/components/scores/RIpiScore.vue"
-import RIssScore from "@/components/scores/RIssScore.vue"
-import Ipi24Score from "@/components/scores/Ipi24Score.vue"
-import IssScore from "@/components/scores/IssScore.vue"
-import LymphomeDeHodgkinScore from "@/components/scores/LymphomeDeHodgkinScore.vue"
+import DataService from "@/service/DataService";
+import CriteresGelfScore from "@/components/scores/CriteresGelfScore.vue";
+import FlipiScore from "@/components/scores/FlipiScore.vue";
+import MipiScore from "@/components/scores/MipiScore.vue";
+import CllBallScore from "@/components/scores/CllBallScore.vue";
+import BinetScore from "@/components/scores/BinetScore.vue";
+import MaladieDeWaldenstromScore from "@/components/scores/MaladieDeWaldenstromScore.vue";
+import MatutesRmhScore from "@/components/scores/MatutesRmhScore.vue";
+import NccnIpiScore from "@/components/scores/NccnIpiScore.vue";
+import SmolderingPrognosisScore from "@/components/scores/SmolderingPrognosisScore.vue";
+import CllIpiScore from "@/components/scores/CllIpiScore.vue";
+import CnsIpiScore from "@/components/scores/CnsIpiScore.vue";
+import RIpiScore from "@/components/scores/RIpiScore.vue";
+import RIssScore from "@/components/scores/RIssScore.vue";
+import Ipi24Score from "@/components/scores/Ipi24Score.vue";
+import IssScore from "@/components/scores/IssScore.vue";
+import LymphomeDeHodgkinScore from "@/components/scores/LymphomeDeHodgkinScore.vue";
 
 export default Vue.extend({
+  components: {
+    CriteresGelfScore,
+    FlipiScore,
+    MipiScore,
+    CllBallScore,
+    BinetScore,
+    MaladieDeWaldenstromScore,
+    MatutesRmhScore,
+    NccnIpiScore,
+    SmolderingPrognosisScore,
+    CllIpiScore,
+    CnsIpiScore,
+    RIpiScore,
+    RIssScore,
+    Ipi24Score,
+    IssScore,
+    LymphomeDeHodgkinScore,
+  },
 
-    components: {
-        CriteresGelfScore,
-        FlipiScore,
-        MipiScore,
-        CllBallScore,
-        BinetScore,
-        MaladieDeWaldenstromScore,
-        MatutesRmhScore,
-        NccnIpiScore,
-        SmolderingPrognosisScore,
-        CllIpiScore,
-        CnsIpiScore,
-        RIpiScore,
-        RIssScore,
-        Ipi24Score,
-        IssScore,
-        LymphomeDeHodgkinScore
-    },
+  data() {
+    return {
+      currentComponent: null,
+    };
+  },
 
-    data()
-    {
-        return {
-            currentComponent: null,
+  mounted() {
+    const slug = this.$route.params.slug;
+    DataService.load()
+      .then(() => {
+        const cat = this.searchBySlug(DataService.$data.tree, slug);
+        console.log(cat);
+
+        if (cat === null) {
+          this.notFound = true;
+        } else {
+          this.currentComponent = cat.component;
         }
-    },
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  },
 
-    mounted()
-    {
-        const slug = this.$route.params.slug
-        DataService.load()
-            .then(() => {
-                const cat = this.searchBySlug(DataService.$data.tree, slug)
-                
-                if (cat === null) {
-                    this.notFound = true
-                } else {
-                    this.currentComponent = cat.component
-                }
-            }).catch(e => {
-                console.log(e)
-            })
-    },
-    
-    methods: {
-        searchBySlug(dataJsonTree: Array<any>, slug: string) {
-            for (let i in dataJsonTree) {
-                const mainCategory = dataJsonTree[i]
-                for (let j in mainCategory.children) {
-                    const subCategory = mainCategory.children[j]
-                    if (subCategory.slug === slug) {
-                        return subCategory
-                    }
-                }
+  methods: {
+    searchBySlug(dataJsonTree: Array<any>, slug: string) {
+      for (let i in dataJsonTree) {
+        const mainCategory = dataJsonTree[i];
+
+        if (mainCategory.children.length>0) {
+          for (let j in mainCategory.children) {
+            const subCategory = mainCategory.children[j];
+            if (subCategory.slug === slug) {
+              return subCategory;
             }
-            return null
+          }
+        }else {
+            if (mainCategory.slug === slug) {
+              return mainCategory;
+            }
         }
-    }
-
-})
+      }
+      return null;
+    },
+  },
+});
 </script>
