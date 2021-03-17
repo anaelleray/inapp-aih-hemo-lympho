48fe5e4ef43ec1e62913054a53a1e21b17fcac01
Najat Filali <filali_najat@hotmail.com>
2021-03-17 11:25:24 +0100

    add component Cll Ball Score

--- src/components/scores/CllBallScore.vue
+++ src/components/scores/CllBallScore.vue
@@ -14,40 +14,37 @@
        
         <div class="question1">
             <div>B2M: Beta-2 microglobulin (B2M) ≥ 5mg/L?</div>
-            <button @click="incrementScore()">>= 5 mg/L </button>
-            <button>&#8249;5 mg/L </button>
+            <button @click="calculateScore(1,1)">≥ 5 mg/L</button>
+            <button  @click="calculateScore(0,1)">&lsaquo; 5 mg/L</button>
         </div>
         <div class="question2" >
             <div>≥ Anemia: Male with Hgb &lsaquo; 120 g/L or Female with Hgb &lsaquo;110 g/L?</div>
-            <button @click="incrementScore()">YES</button>
-            <button>NO</button>
+            <button @click="calculateScore(1,2)">YES</button>
+            <button @click="calculateScore(0,2)">NO</button>
         </div>
         <div class="question3">
             <div>LDH: LDH > upper limit of normal?</div>
-            <button @click="incrementScore()">Elevated</button>
-            <button> Not Elevated</button>
+            <button @click="calculateScore(1,3)">Elevated</button>
+            <button @click="calculateScore(0,3)"> Not Elevated</button>
         </div>
         <div class="question4">
             <div>Last therapy: Time from initiation of last therapy &lsaquo; 24 months?</div>
-            <button @click="incrementScore()">&lsaquo; 24 months</button>
-            <button> >= 24 months </button>
+            <button @click="calculateScore(1,4)">&lsaquo; 24 months</button>
+            <button @click="calculateScore(0,4)">≥ 24 months </button>
         </div>
         <button @click="calculateResult()"> View results </button>
-        <div class="results">
-            <h3>Results</h3>
+        <div class="result" id="result">
+            <h3>Result</h3>
             <div>Risk</div>
             <div>{{result}}</div>
             <div>
                 <h3>Interpretation</h3>
                 <div>The Relapsed CLL BALL Score identifies three prognostic groups with significantly different overall survival. Application of this risk model to patients with relapsed/refractory CLL reliably identifies patients at increased risk of death, and is validated for patients treated with all currently approved targeted therapies: ibrutinib, idelalisib, and venetoclax.
-<br/>By applying the Relapsed CLL BALL Score to individual patients, treating physicians can identify patients at increased risk of death despite targeted therapies. We propose that high and intermediate risk patients should be evaluated for consideration in clinical trials, as their expected survival with modern targeted therapies remains suboptimal.
-<br/>Investigators can address this unmet need by designing prospective trials targeting these higher risk patients.</div>
+                <br/>By applying the Relapsed CLL BALL Score to individual patients, treating physicians can identify patients at increased risk of death despite targeted therapies. We propose that high and intermediate risk patients should be evaluated for consideration in clinical trials, as their expected survival with modern targeted therapies remains suboptimal.
+                <br/>Investigators can address this unmet need by designing prospective trials targeting these higher risk patients.</div>
             </div>
-
-
         </div>
-        
-  </div>
+    </div>
 </template>
 
 <script lang="ts">
@@ -59,44 +56,37 @@ export default Vue.extend({
     return {
         result :"", 
         score: 0,
+        question1:0,
+        question2:0,
+        question3:0,
+        question4:0,
     }
-  },
-  mounted()
-  {
-      
   },
   methods: {
-   incrementScore(){
-        this.score ++
-        console.log(this.score)
+   calculateScore(value, question){
+       switch(question){
+           case 1 : this.question1 = value;break
+           case 2 : this.question2 = value;break
+           case 3 : this.question3 = value;break
+           case 4 : this.question4 = value;break
+       }
+        this.score = this.question1 + this.question2 + this.question3 + this.question4
     },
-
-    displayQuestion(question){
-        question.display
-    }, 
-
-    isClicked(){
-
-    }, 
     calculateResult (){
-      if (this.score <= 1)  this.result ="Low Risk"
-      if (this.score >= 2 && this.score <=3 ) this.result = "Intermediate Risk"
-      if (this.score == 4) this.result = "High Risk"
-      console.log(this.result)
+        var res = document.getElementById("result")
+        res.style.display = "block"
+        if (this.score <= 1)  this.result ="Low Risk"
+        if (this.score >= 2 && this.score <=3 ) this.result = "Intermediate Risk"
+        if (this.score == 4) this.result = "High Risk"
     }
   }
-
 })
 </script>
 
 <style>
-.question2{
-    display: none;
+
+.result {
+    display:none;
 }
-.question3{
-    display: none;
-}
-.question4{
-    display: none;
-}
+
 </style>
