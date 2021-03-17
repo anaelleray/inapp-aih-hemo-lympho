33e231cc94659d19ac714e5b57e2ddc12068051f
nerses <nerseshak@gmail.com>
2021-03-17 12:38:02 +0100

    LymphomeDuMaltScore done

--- src/components/scores/LymphomeDuMaltScore.vue
+++ src/components/scores/LymphomeDuMaltScore.vue
@@ -1,13 +1,54 @@
 <template>
-  
+  <div class="score-1">
+      <h2>MALT Lymphoma prognosis (MALT-IPI)</h2>
+      <h3>Estimate prognosis in MALT lymphoma</h3>
+      <br>
+      <div>
+          <table>
+            <tr>
+                <td>1. Age?</td>
+                  <td>
+                    <button style="background:#c2f0c2"><70 years</button>
+                </td>
+                <td>
+                    <button style="background:#c2f0c2">≥70 years</button>
+                </td>
+            </tr>
+            <tr>
+                <td>2. LDH?</td>
+                  <td>
+                    <button style="background:#c2f0c2">Normal</button>
+                </td>
+                <td>
+                    <button style="background:#c2f0c2">Elevated</button>
+                </td>
+            </tr>
+            <tr>
+                <td>3. Stage?</td>
+                  <td>
+                    <button style="background:#c2f0c2">Stage I/II</button>
+                </td>
+                <td>
+                    <button style="background:#c2f0c2">Stage III/IV</button>
+                </td>
+            </tr>
+        </table>
+    </div>
+    <input type="text" value="0">
+  </div>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "LymphomeDuMaltScore",
+  data() {
+    return {};
+  },
 
-}
+  mounted() {},
+});
 </script>
 
 <style>
-
 </style>
