0da9590dbc69846ee71d7fb74e30037ca573ff14
nerses <nerseshak@gmail.com>
2021-03-17 09:59:24 +0100

    first test commit

--- src/components/scores/MaladieDeWaldenstromScore.vue
+++ src/components/scores/MaladieDeWaldenstromScore.vue
@@ -1,5 +1,39 @@
 <template>
-  
+  <div class="score-1">
+      <h2>AAAAAAAAAAAAAAAA</h2>
+      <div>
+          <table>
+            <tr>
+                <td>BBBBBBBB</td>
+                  <td>
+                    <button style="background:green">>3</button>
+                </td>
+                <td>
+                    <button style="background:green">inf3</button>
+                </td>
+            </tr>
+            <tr>
+                <td>Anemia, Hgb inf10 g/dL </td>
+                  <td>
+                    <button style="background:green">No</button>
+                </td>
+                <td>
+                    <button style="background:green">Yes</button>
+                </td>
+            </tr>
+            <tr>
+                <td>Thrombocytopenia, Platelets inf100,000/mm³</td>
+                  <td>
+                    <button style="background:green">No</button>
+                </td>
+                <td>
+                    <button style="background:green">Yes</button>
+                </td>
+            </tr>
+        </table>
+    </div>
+    <input type="text" value="0">
+  </div>
 </template>
 
 <script>
