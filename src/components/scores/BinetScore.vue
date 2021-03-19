ddadf2e74fc7c78550645b676d53ed73908c69ae
Najat Filali <filali_najat@hotmail.com>
2021-03-19 11:58:09 +0100

    add links in binetScore

--- src/components/scores/BinetScore.vue
+++ src/components/scores/BinetScore.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="score">
+    <div class="Binet-score">
 
         <div class="title-red">
             Binet Staging System for Chronic Lymphocytic Leukemia (CLL)
@@ -53,6 +53,23 @@
                 </div>
             </div>
         </div>
+        <div class="sous-title">
+            <div>
+                LITERRATURE<br>
+                ORIGINAL/PRIMARY REFERENCE
+            </div>
+            <div>
+                <a href="https://www.ncbi.nlm.nih.gov/pubmed/890666" target="_blank">Binet JL, Leporrier M, Dighiero G, et al. A clinical staging system for chronic lymphocytic leukemia: prognostic significance. Cancer. 1977;40(2):855-64.</a>
+            </div>
+        </div>
+        <div class="sous-title">
+            <div>
+                OTHER REFERENCES
+            </div>
+            <div>
+                <a href="https://pubmed.ncbi.nlm.nih.gov/25461996/" target="_blank">Nabhan C, Rosen ST. Chronic lymphocytic leukemia: a clinical review. JAMA. 2014;312(21):2265-76.</a>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -60,9 +77,6 @@
 import Vue from "vue"
 export default Vue.extend({  
     name: "BinetScore",
-    components: {
-
-    },
     data()
     {
         return {
@@ -113,10 +127,6 @@ export default Vue.extend({
                 return "button "
             }
         }       
-        
-    },
-    mounted()
-    {
         
     },
 })
